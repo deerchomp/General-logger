fbf2d108edb304811f3765729a06c6368027bf97
Nrno <iamnarnoo@gmail.com>
2019-09-11 14:52:26 -0700

    adding sampler file

---
+++ sampler.cpp
@@ -0,0 +1,10 @@
+#include "Log.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hi there! Let's test out our log class." << endl;
+    cout << "*****************************************" << endl;
+    return 0;
+}
